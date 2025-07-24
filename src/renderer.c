nxtedition/libplacebo

commit bf6978e825a6dc14eb6afceb3e4abeb28ba6ae59

renderer: properly avoid alpha mode double application
This correctly fixes the issue that commit 0067206f was trying to address.

src/renderer.c
@@ -2469,13 +2469,20 @@ static bool pass_output_target(struct pass_state *pass)
         }
     }
 
-    if (img->comps == 4 && has_alpha)
-        pl_shader_set_alpha(sh, &img->repr, target->repr.alpha);
 
     // Apply the color scale separately, after encoding is done, to make sure
     // that the intermediate FBO (if any) has the correct precision.
     struct pl_color_repr repr = target->repr;
     float scale = pl_color_repr_normalize(&repr);
+
+    // If the alpha mode is already applied, don't double-apply it
+    if (img->repr.alpha == repr.alpha || !has_alpha || img->comps == 4) {
+        repr.alpha = PL_ALPHA_NONE;
+    } else {
+        // `pl_shader_encode_color` expects independent alpha
+        pl_shader_set_alpha(sh, &img->repr, PL_ALPHA_INDEPENDENT);
+    }
+
     enum pl_lut_type lut_type = guess_frame_lut_type(target, true);
     if (lut_type != PL_LUT_CONVERSION)
         pl_shader_encode_color(sh, &repr);

commit d3913c04eae0774520ac02e3b8c93b66bb95470e

renderer: fix incorrect condition on alpha check
This was incorrectly inverted during bf6978e82. Additionally, while reasoning
through the logic, I realized that the `!has_alpha` case is not really
necessary (since it's usually handled by the blend pass above).

Fixes: bf6978e825a6dc14eb6afceb3e4abeb28ba6ae59

src/renderer.c
@@ -2477,7 +2477,7 @@ static bool pass_output_target(struct pass_state *pass)
     float scale = pl_color_repr_normalize(&repr);
 
     // If the alpha mode is already applied, don't double-apply it
-    if (img->repr.alpha == repr.alpha || !has_alpha || img->comps == 4) {
+    if (img->repr.alpha == repr.alpha || img->comps < 4) {
         repr.alpha = PL_ALPHA_NONE;
     } else {
         // `pl_shader_encode_color` expects independent alpha